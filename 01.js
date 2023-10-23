38fec8bc115527f197154113b37f7e9c8b7e1a71
manbeao <lesles0906@naver.com>
2023-10-23 16:19:13 +0900

    4th commit

---
+++ 01.js
@@ -0,0 +1,10 @@
+// $(()=>{
+//     $(window).on('mousemove',function(e){
+//         console.log(e);
+//         let x=e.clientX;
+//         let y=e.clientY;
+//        $('.hover img').css({right:(y-5),left:(x+5),top:(y-5),bottom:(x+5)});
+//     });
+
+    
+// })
