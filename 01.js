60af20cc40741b5c22139e6652d710ac5ed14100
manbeao <lesles0906@naver.com>
2023-10-23 17:11:48 +0900

    5th commit

--- 01.js
+++ 01.js
@@ -1,3 +1,28 @@
+// const win = $(window);
+// const speed = win.height() * 0.9;
+// const pj = $('.project');
+// win.on('scroll', function () {
+// 	let winSCT = win.scrollTop() + speed;
+// 	pj.each(function (i, o) {
+//     const tg=$(this);
+//     const tgtop=tg.offset().top;
+//     //console.log(tg.offset().top);
+//     if(winSCT>tgtop){
+//       console.log((winSCT>pj.eq(1).offset().top));
+//       tg.find('.left_box').css('transform','translateX(0%)');
+//       tg.find('.right_box').css('transform','translateX(0%)');
+//     }
+// 	});
+// });
+
+
+
+
+
+
+
+
+
 // $(()=>{
 //     $(window).on('mousemove',function(e){
 //         console.log(e);
