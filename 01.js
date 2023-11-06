28cc5ab6ce30508d2794a4f1677f6942989428f3
manbeao <lesles0906@naver.com>
2023-11-06 14:39:13 +0900

    first commit

--- 01.js
+++ 01.js
@@ -1,11 +1,30 @@
 $('.home').on('mousemove', function (e) {
-	console.log(e);
 	let x = e.pageX - 15 + "px";
 	let y = e.pageY - 15 + "px";
 	$('.cusor').css({ left: + x, top: + y });
 });
 
 
+
+$('#content1.carousel').slick({ slidesToShow: 2 });
+
+$('.tab-button').on('click', function () {
+	const tabButtonId = $(this).attr('id');
+	$('.tab-button').removeClass('active');
+	$('.tab-content').removeClass('active');
+
+	$(this).addClass('active');
+  //변수tabButtonId 의 값을 이용하여 content1 로 편집
+	const targetContent = $('#' + 'content' + tabButtonId.slice(-1));
+	targetContent.addClass('active');
+
+	if (targetContent.hasClass('slick-initialized')) {
+		targetContent.slick('unslick');
+	}
+	targetContent.slick({ slidesToShow: 2 });
+});
+
+$('.popup').magnificPopup({ type: 'image' });
 
 
 
