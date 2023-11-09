a32ee45ddf4453f0c2181ab243f6d18583c8d557
manbeao <lesles0906@naver.com>
2023-11-09 13:18:57 +0900

    14th commit

--- javascript/01.js
+++ javascript/01.js
@@ -1,10 +1,8 @@
 $('.home').on('mousemove', function (e) {
-	let x = e.pageX - 15 + "px";
-	let y = e.pageY - 15 + "px";
-	$('.cusor').css({ left: + x, top: + y });
+	let x = e.pageX - 15 + 'px';
+	let y = e.pageY - 15 + 'px';
+	$('.cusor').css({ left: +x, top: +y });
 });
-
-
 
 $('#content1.carousel').slick({ slidesToShow: 2 });
 
@@ -14,7 +12,7 @@ $('.tab-button').on('click', function () {
 	$('.tab-content').removeClass('active');
 
 	$(this).addClass('active');
-  //변수tabButtonId 의 값을 이용하여 content1 로 편집
+	//변수tabButtonId 의 값을 이용하여 content1 로 편집
 	const targetContent = $('#' + 'content' + tabButtonId.slice(-1));
 	targetContent.addClass('active');
 
@@ -26,108 +24,57 @@ $('.tab-button').on('click', function () {
 
 $('.popup').magnificPopup({ type: 'image' });
 
-
-
 // ---------------------------------------------------
 /* skill */
-// $(function () {
-// 	var sboxs = $('.s_boxs');
-// 	var animationOst = $('.animation').offset().top - 600;
-// 	var isAni = false;
-// 	$(window).scroll(function () {
-// 		if ($(window).scrollTop() >= animationOst && !isAni) {
-// 			progressAnimation();
-// 		}
-// 	});
-
-// 	function progressAnimation() {
-// 		sboxs.each(function () {
-// 			var $this = $(this),
-// 				progressText = $this.find('.rate'),
-// 				progressRate = progressText.attr('data-rate');
-// 			progressBar.animate({ width: progressRate + '%' }, 2500);
-// 			// console.log(progressText);
-// 			var text = function () {
-// 				$({ rate: 0 }).animate(
-// 					{ rate: progressRate },
-// 					{
-// 						duration: 2000,
-// 						progress: function () {
-// 							var now = this.rate;
-// 							console.log(now);
-// 							progressText.text(Math.floor(now) + '%');
-// 						},
-// 						complete: function () {
-// 							isAni = true;
-// 						},
-// 					}
-// 				);
-// 			};
-// 			text();
-// 		});
-// 	}
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ----------------------------------------------
-/* slide */
-
-
 const win = $(window);
 const speed = win.height() * 0.9;
 const project = $('.project');
+const animationOst = $('.container2').offset().top;
+
+let sboxs = $('.s_boxs');
+let isAni = false;
+
+function progressAnimation() {
+	sboxs.each(function () {
+		var $this = $(this),
+			progressText = $this.find('.rate'),
+			progressRate = progressText.attr('data-num');
+
+		var text = function () {
+			$({ rate: 0 }).animate(
+				{ rate: progressRate },
+				{
+					duration: 2000,
+					progress: function () {
+						var now = this.rate;
+						progressText.text(Math.floor(now) + '%');
+					},
+					complete: function () {
+						isAni = true;
+					},
+				}
+			);
+		};
+		text();
+	});
+}
+
+/* slide */
+
 win.on('scroll', function () {
 	let winSCT = win.scrollTop() + speed;
+	if (winSCT >= animationOst && !isAni) {
+		progressAnimation();
+	}
 	project.each(function (i, o) {
 		const tg = $(this);
 		const tgtop = tg.offset().top;
-		//console.log(tg.offset().top);
 		if (winSCT > tgtop) {
 			tg.find('.left_box').css('transform', 'translateX(0%)');
 			tg.find('.right_box').css('transform', 'translateX(0%)').delay(5000);
 		}
 	});
 });
-
-// ---------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // const win = $(window);
 // const speed = win.height() * 0.9;
@@ -145,11 +92,3 @@ win.on('scroll', function () {
 //     }
 // 	});
 // });
-
-
-
-
-
-
-
-
